7e8e0c980cc2b3de39334c8c1cb92a91884c5a7f
Trey Dempsey <trey.dempsey@gmail.com>
2023-03-27 08:12:14 -0500

    day08: Added solution

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -6,8 +6,8 @@ use nom::IResult;
 
 use std::collections::HashSet;
 use std::env;
-use std::io;
 use std::fs;
+use std::io;
 use std::path::Path;
 
 fn main() -> Result<(), io::Error> {
@@ -20,13 +20,19 @@ fn main() -> Result<(), io::Error> {
         Ok(r) => println!("{}", r),
         Err(e) => println!("Error: {}", e),
     }
-    //println!("part2: {}", part2(&input));
+    match part2(&input) {
+        Ok(r) => println!("{}", r),
+        Err(e) => println!("Error: {}", e),
+    }
     Ok(())
 }
 
 fn line(input: &str) -> IResult<&str, Vec<u8>> {
     let (input, (row, _)) = tuple((digit1, opt(newline)))(input)?;
-    let chars = row.chars().flat_map(|c| c.to_digit(10).and_then(|d| u8::try_from(d).ok())).collect();
+    let chars = row
+        .chars()
+        .flat_map(|c| c.to_digit(10).and_then(|d| u8::try_from(d).ok()))
+        .collect();
     Ok((input, chars))
 }
 
@@ -41,17 +47,17 @@ fn part1(input: &str) -> Result<String, nom::Err<nom::error::Error<&str>>> {
     let rows = forest.len();
     let cols = forest[0].len();
 
-    for r in 1..(rows-1) {
+    for r in 1..(rows - 1) {
         let mut tallest = forest[r][0];
-        for c in 1..(cols-1) {
+        for c in 1..(cols - 1) {
             if forest[r][c] > tallest {
                 tallest = forest[r][c];
                 visible.insert((c, r));
             }
         }
 
-        let mut tallest = forest[r][cols-1];
-        for c in (1..(cols-1)).rev() {
+        let mut tallest = forest[r][cols - 1];
+        for c in (1..(cols - 1)).rev() {
             if forest[r][c] > tallest {
                 tallest = forest[r][c];
                 visible.insert((c, r));
@@ -59,17 +65,17 @@ fn part1(input: &str) -> Result<String, nom::Err<nom::error::Error<&str>>> {
         }
     }
 
-    for c in 1..(cols-1) {
+    for c in 1..(cols - 1) {
         let mut tallest = forest[0][c];
-        for r in 1..(rows-1) {
+        for r in 1..(rows - 1) {
             if forest[r][c] > tallest {
                 tallest = forest[r][c];
                 visible.insert((c, r));
             }
         }
 
-        let mut tallest = forest[rows-1][c];
-        for r in (1..(rows-1)).rev() {
+        let mut tallest = forest[rows - 1][c];
+        for r in (1..(rows - 1)).rev() {
             if forest[r][c] > tallest {
                 tallest = forest[r][c];
                 visible.insert((c, r));
@@ -78,6 +84,56 @@ fn part1(input: &str) -> Result<String, nom::Err<nom::error::Error<&str>>> {
     }
 
     Ok((2 * (rows - 1) + 2 * (cols - 1) + visible.len()).to_string())
+}
+
+fn part2(input: &str) -> Result<String, nom::Err<nom::error::Error<&str>>> {
+    let (_, forest) = parse_input(input)?;
+    let rows: i32 = forest.len().try_into().unwrap();
+    let cols: i32 = forest[0].len().try_into().unwrap();
+    let mut best = 0;
+
+    for r in 0..rows {
+        for c in 0..cols {
+            let right = viewing_distance(&forest, r, c, 0, 1);
+            let down = viewing_distance(&forest, r, c, 1, 0);
+            let left = viewing_distance(&forest, r, c, 0, -1);
+            let up = viewing_distance(&forest, r, c, -1, 0);
+            if (right * down * left * up) > best {
+                best = right * down * left * up;
+            }
+        }
+    }
+
+    Ok(best.to_string())
+}
+
+fn viewing_distance(forest: &Vec<Vec<u8>>, start_r: i32, start_c: i32, dr: i32, dc: i32) -> i32 {
+    let rows: i32 = forest.len().try_into().unwrap();
+    let cols: i32 = forest[0].len().try_into().unwrap();
+    let treehouse_height = forest[start_r as usize][start_c as usize];
+    let mut distance = 0;
+    let mut r: i32 = start_r;
+    let mut c: i32 = start_c;
+
+    loop {
+        // Move in the direction of travel
+        r += dr;
+        c += dc;
+
+        // Stop iterating when we hit an edge
+        if r < 0 || r >= rows || c < 0 || c >= cols {
+            break;
+        }
+
+        distance += 1;
+
+        // Stop iterating if the tree is the same or taller than us
+        if treehouse_height <= forest[r as usize][c as usize] {
+            break;
+        }
+    }
+
+    distance
 }
 
 #[cfg(test)]
@@ -93,5 +149,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!("21", part1(INPUT).unwrap());
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!("8", part2(INPUT).unwrap());
     }
 }
